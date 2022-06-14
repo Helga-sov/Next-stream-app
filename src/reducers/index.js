3e86946d3301b80ac61710f6684f0770dc31003d
Olga Sandu <sandu.helga@gmail.com>
2022-06-14 18:46:47 +0200

    delete src

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import authReducer from "./authReducer";
-import streamReducer from "./streamReducer";
-
-export default combineReducers({
-  auth: authReducer,
-  streams: streamReducer,
-});
